084236ff382a7985d4ee5617aa346ac8bf65bac1
cschafer <cschafer@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2008-11-18 03:53:16 +0000

    git-svn-id: http://subversion.assembla.com/svn/avr_work@2 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ avr065/defines.h
@@ -0,0 +1,31 @@
+/* This file has been prepared for Doxygen automatic documentation generation.*/
+/*! \file *********************************************************************
+ *
+ * \brief
+ *      Defines and includes for main.c and LCD_driver.c
+ *
+ * \par Application note:
+ *      AVR065: LCD Driver for the STK502
+ *
+ * \par Documentation:
+ *      For comprehensive code documentation, supported compilers, compiler
+ *      settings and supported devices see readme.html
+ *
+ * \author
+ *      Atmel Corporation: http://www.atmel.com \n
+ *      Support email: avr@atmel.com
+ *
+ * $Name: RELEASE_1_1 $
+ * $Revision: 1.3 $
+ * $RCSfile: defines.h,v $
+ * $Date: 2006/02/16 18:14:01 $  \n
+ ******************************************************************************/
+
+
+// Includes
+	#define SFR_W_EXT( a, b )
+    #include <iom169.h>
+
+    #define FALSE   0
+    #define TRUE    1
+
