2b467536fb61ba1edfdb0cdea1a58db04c5c35d0
cschafer <cschafer@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-01-18 06:29:00 +0000

    Work on i2c twi interface begun.
    git-svn-id: http://subversion.assembla.com/svn/avr_work@72 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- breadline/twi_i2c.h
+++ breadline/twi_i2c.h
@@ -2,13 +2,25 @@
 #ifndef _TWI_I2C_H_
 #define _TWI_I2C_H_
 
-#define SCL_freq 100000 //100kHz
+#define F_SCL 400000 //400kHz
+
+// Slave Address(s) for device
+#define I2C_SLAVE_ADDR		0b0011100
+#define I2C_SLAVE_ADDR_MSK	0b0000000
+
+// Respond to General Call in Slave mode?
+#define I2C_GENERAL_CALL_EN 0
 
 
-#define SCL_freq_calc (F_CPU/(16+2(TWI_br)*pow(4,TWI_ps)))
+// I2C Clock Generation
+#define TWI_PS_MSK 0
+#if   (F_SCL == 400000 //400kHz
+#define TWI_BR_VAL 2
+#elif (F_SCL == 100000 //100kHz
+#define TWI_BR_VAL 32
+#endif
 
-
-void twi_i2c_init(void);
+void twi_init(void);
 
 
 
