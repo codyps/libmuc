98e581ebdfc67dcfe260cf44ff343b361801152b
cschafer <cschafer@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-06 18:38:45 +0000

    servo_fpwm_cyclic compiles and is almost generalized for any timer with a single change (currently needs 2 changhes :(, macros dislike me ) project compiles. no run time testing done.
    git-svn-id: http://subversion.assembla.com/svn/avr_work@124 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ magellan2/servo_conf.h
@@ -0,0 +1,45 @@
+
+//FIXME: Currently the following line does nothing, had to place the '5' directly in the S_# macros
+#define S_TIMER	5
+
+#define S_A(_A)	COMB2(_A, 5 )
+#define S_I(_A,_B) COMB3(_A, 5 ,_B)
+
+#define COMB3(_A,_num,_C) _A ## _num ## _C
+#define COMB2(_A,_num) _A ## _num
+
+
+
+// Registers
+//  Bits
+#define SERVO_TCCRB	S_I(TCCR,B)
+ #define WGM3		S_I(WGM,3)
+ #define WGM2		S_I(WGM,2)
+
+#define SERVO_TCCRA	S_I(TCCR,A)
+ #define WGM1		S_I(WGM,1)
+ #define WGM0		S_I(WGM,0)
+ #define CS2		S_I(CS,2)
+ #define CS1		S_I(CS,1)
+ #define CS0		S_I(CS,0)
+
+#define SERVO_TIMSK	S_A(TIMSK)
+ #define OCIEA		S_I(OCIE,A)
+ #define OCIEB		S_I(OCIE,B)
+ #define OCIEC		S_I(OCIE,C)
+ #define TOIE		S_A(TOIE)
+
+#define SERVO_ICR	S_A(ICR)
+#define SERVO_TCNT	S_A(TCNT)
+
+#define SERVO_OCRA	S_I(OCR,A)
+#define SERVO_OCRB	S_I(OCR,B)
+#define SERVO_OCRC	S_I(OCR,C)
+
+// Interrupts
+#define TIMER_S_OVF_vect S_I(TIMER,_OVF_vect)
+
+#define TIMER_S_COMPA_vect S_I(TIMER,_COMPA_vect)
+#define TIMER_S_COMPB_vect S_I(TIMER,_COMPB_vect)
+#define TIMER_S_COMPC_vect S_I(TIMER,_COMPC_vect)
+
