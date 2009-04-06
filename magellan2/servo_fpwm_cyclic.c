419b888be231703cfd33fabbdfa0ad5025c4388c
cschafer <cschafer@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-04-06 18:38:47 +0000

    servo_fpwm_cyclic working w/ generalization. need to fix some of the macros. project compiles, no runtime testing.
    git-svn-id: http://subversion.assembla.com/svn/avr_work@125 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- magellan2/servo_fpwm_cyclic.c
+++ magellan2/servo_fpwm_cyclic.c
@@ -10,6 +10,7 @@
 #include <util/atomic.h>
 
 #include "servo.h"
+#include "servo_conf.h"
 #include "util.h"
 
 // Time until overflow (in ms)
@@ -23,20 +24,21 @@ uint8_t servo_index [SERVO_AMOUNT];
 /* (end) */
 
 
-void timer5_fpwm_start(void);
+void timer_servo_init(void);
 void servo_pin_init(void);
 
 uint8_t servo_set(uint16_t servo_val, uint8_t servo_number) {
 	if (servo_val>CLICKS_MS(3) || servo_val<CLICKS_MS(1) || servo_number>SERVO_AMOUNT) 
 		return -1;
 	servo_position[servo_number] = servo_val;
+	return 0;
 }
 
 void init_servos(void) {
 	servo_pin_init();	
-	memset_16( servo_values, CLICKS_MS(1)/2+CLICKS_MS(1), SERVO_AMOUNT);
-	update_servos();
-	timer5_init();
+	memset_16( servo_position, CLICKS_MS(1)/2+CLICKS_MS(1), SERVO_AMOUNT);
+	//update_servos();
+	timer_servo_init();
 }
 
 //TODO: This should be assigned at compile time, not run time.
@@ -55,40 +57,42 @@ void servo_pin_init(void) {
 	servo_index[SERVO_IRR] = SERVO_IRR_INDEX;
 }
 
-void timer5_init(void) {
-	power_timer5_enable();	
 
-	// Fast PWM, ICR5 = TOP
-	//WGM5[3,2,1,0] = 1,1,1,0
-	TCCR5B = (1<<WGM3)|(1<<WGM2); //(disables timer, CS[2,1,0] = 0		)
-	TCCR5A = (1<<WGM1)|(0<<WGM0); //(disables outputs, COM[A,B,C][1,0] = 0	)
-	
-	//TIMSK5 = (1<<OCIE5A)|(1<<OCIE5B)|(1<<OCIE5A)|(1<<TOIE5);
-	TIMSK5 = (1<<OCIE5A)|(1<<TOIE5);
-	// write the 16 bit registers.
-	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-		ICR5 = CLICKS_MS(TIMER_PERIOD);	
-		TCNT5 = 0;
-	
-		//OCR5A = ?;
-		//OCR5B = ?;
-		//OCR5C = ?;
-	};
-
-	// Prescale & Start.
-	TCCR5B|= TIMER_PRESCALE_1;
-}
-
-#define SERVO_PIN_LOW(_servo_) servo_port[_servo_] &= (uint8_t) ~(1<<servo_index[_servo_])
-#define SERVO_PIN_HIGH(_servo_) servo_port[_servo_] |= (1<<servo_index[_servo_])
+#define SERVO_PIN_LOW(_servo_) ( *servo_port[_servo_] &= (uint8_t) ~( 1<<servo_index[_servo_] ) )
+#define SERVO_PIN_HIGH(_servo_) ( *servo_port[_servo_] |= (uint8_t) (1<<servo_index[_servo_]) )
 
 #define max(a,b) (b<a)?a:b
 #define min(a,b) (b>a)?a:b
 
 static uint8_t cycle; //= 0;
 
+void timer_servo_init(void) {
+	power_timer5_enable();	
+
+	// Fast PWM, ICR5 = TOP
+	//WGM5[3,2,1,0] = 1,1,1,0
+	SERVO_TCCRB = (1<<WGM3)|(1<<WGM2); //(disables timer, CS[2,1,0] = 0		)
+	SERVO_TCCRA = (1<<WGM1)|(0<<WGM0); //(disables outputs, COM[A,B,C][1,0] = 0	)
+	
+	//TIMSK5 = (1<<OCIEA)|(1<<OCIEB)|(1<<OCIEC)|(1<<TOIE);
+	SERVO_TIMSK = (1<<OCIEA)|(1<<TOIE);
+	// write the 16 bit registers.
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+		SERVO_ICR = CLICKS_MS(TIMER_PERIOD);	
+		SERVO_TCNT = 0;
+	
+		SERVO_OCRA = servo_position[cycle];
+	};
+
+	// We don't get the first OVF isr, so pull it high now.
+	SERVO_PIN_HIGH(cycle);
+	
+	// Prescale & Start.
+	SERVO_TCCRB|= TIMER_PRESCALE_1;
+}
+
 // Needs to spend less than 1ms, F_CPU/1000 ops. (16e3@16e6Hz)
-ISR(TIMER5_OVF_vect) {
+ISR(TIMER_S_OVF_vect) {
 
 	cycle = (cycle+1)%(20/TIMER_PERIOD);
 
@@ -96,22 +100,22 @@ ISR(TIMER5_OVF_vect) {
 		// set servo 'cycle' pin(s) high
 		SERVO_PIN_HIGH(cycle);
 		// set the OCR5A appropratly
-		OCR5A = servo_value[cycle];
+		SERVO_OCRA = servo_position[cycle];
 	}
-	else // if (cycle == 4) {
+	else {// if (cycle == 4) {
 		//???
 	}
 }
 
-ISR(TIMER5_COMPA_vect) {
+ISR(TIMER_S_COMPA_vect) {
 	SERVO_PIN_LOW(cycle);
 }
-
-ISR(TIMER5_COMPB_vect) {
+/*
+ISR(TIMER_S_COMPB_vect) {
 	
 }
 
-ISR(TIMER5_COMPC_vect) {
+ISR(TIMER_S_COMPC_vect) {
 	
 }
-
+*/
--- magellan2/util.c
+++ magellan2/util.c
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 int cmp_16(const void * a, const void * b) {
 	return *( (uint16_t *) a) - *( (uint16_t *) b);
 }
@@ -5,6 +7,6 @@ int cmp_16(const void * a, const void * b) {
 void memset_16(uint16_t * dest, uint16_t val, uint8_t len) {
         do { 
 		len--;
-		dest[i] = len    
-	} while (i!=0);
+		dest[len] = val;
+	} while (len!=0);
 }
