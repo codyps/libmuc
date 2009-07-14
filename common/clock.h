1578d1475ffab26e432795949455c2d7bf49c50f
Not Important <not@not-laptop.(none)>
2009-07-14 18:25:28 -0400

    Add MHz and KHz unit converters

--- common/clock.h
+++ common/clock.h
@@ -1,6 +1,8 @@
 #ifndef _CLOCK_H_
 #define _CLOCK_H_
 
+#define MHz(x) ( x * 1000000 )
+#define KHz(x) ( x *    1000 )
 
 #define CLICKS_MS(_ms_) ( (uint16_t) ( F_CPU * (_ms_) / 1000 ) )
 #define CLICKS_US(_us_) ( (uint16_t) ( F_CPU / 1000000 * (_us_) ) ) // 1000000 = 1e6
