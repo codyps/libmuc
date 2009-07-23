f429bb770b67d882c15741cc9e34281d3618d017
Not Important <not@not-laptop.(none)>
2009-07-23 03:05:26 -0400

    Main now prints out the adc values over the spi link as soon as all 4 are refreshed (delay of 200 ms)

--- lline/main.c
+++ lline/main.c
@@ -9,6 +9,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 #include <avr/io.h>
 #include <avr/power.h>
@@ -39,10 +41,24 @@ ISR(BADISR_vect){
 
 int main(void) {
 	init();
+
+	uint16_t adc_val[ADC_CHANNEL_CT];
+	
      
     	for(;;) {
-          spi_puts("HELLO\n");
-		_delay_ms(1000);
+		if (adc_new_data) {
+			adc_new_data = false;
+			memcpy(adc_val, (uint16_t *) adc_values,sizeof(adc_val));
+			for (uint8_t i = 0; i < ADC_CHANNEL_CT; i++) {			
+				spi_putchar(i+'0');
+				spi_putchar(' ');
+				spi_putchar(':');
+				spi_puth2(adc_val[i]);
+				spi_putchar('\t');
+			}
+			spi_putchar('\n');
+		}
+		_delay_ms(200);
 	}
 	return 0;
 }
