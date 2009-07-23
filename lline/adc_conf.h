0764d575059956f263dede4805445407e06b0cd3
Not Important <not@not-laptop.(none)>
2009-07-22 22:53:11 -0400

    bit of tidying

--- lline/adc_conf.h
+++ lline/adc_conf.h
@@ -6,24 +6,16 @@
 
 #include "clock.h"
 
-
 // ADC Prescale calculation (2^n | uint n < 8 )
-#define ADC_MAX_CLK KHz(200L)
+#define ADC_MAX_CLK KHz(200L)	// From datasheet.
 
 #define ADC_PRESCALE_BITS ( (uint8_t) ceil( log((float)F_CPU/ADC_MAX_CLK)/log(2) ) )
-/*
-#if (F_CPU==MHz(8))
-#define ADC_PRESCALE_BITS 6 // for 8MHz
-#else
-#error "ADC Presacle bits undetermined"
-#endif
-*/
 
 #define ADC_PRESCALE  ( (uint8_t) pow(2,ADC_PRESCALE_BITS) )
 #define ADC_F (F_CPU/ADC_PRESCALE)
 #define ADC_CYCLE ( F_CPU / ADC_F ) // == ADC_PRESCALE
 
 const static uint8_t adc_channels [] = { 0, 4, 5, 6 };
-#define ADC_CHANNEL_CT sizeof(adc_channels)
+#define ADC_CHANNEL_CT ( sizeof(adc_channels) )
 
 #endif /*_ADC_CONF_H_*/
