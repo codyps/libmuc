a78038ccbbf388c1a378275993739b7c21912eca
Not Important <not@not-laptop.(none)>
2009-07-24 17:41:14 -0400

    The motor config for the previous import

--- lline/motor_conf.h
+++ lline/motor_conf.h
@@ -10,37 +10,25 @@ PB4 -> BIN2
 M1/A -> right
 M2/B -> left
 */
+#include <math.h>
+#include <stdint.h>
+
+#define MOTOR_T1_PRESCALE 1
+#define T1_PRESCALE_BITS(prescale)	\
+	((uint8_t)(  0x0F & (uint8_t)(( log(prescale) + log(2) )/log(2)) ))
 
 
-// A / B defines (specified)
-#define M_PWMA OCR1D
-#define M_PWMB OCR1B
+#define USE_OC1A 0
+#define USE_OC1B 1 
+#define USE_OC1D 1
 
-#define M_PWMA_PIN 5
-#define M_PWMB_PIN 3
+#ifdef USE_OC1A_PWM
+#define T1A_1 1
+#define T1A_0 0
+#define T1A_P 1
+#endif
 
-#define MOTOR_PWM_PORT PORTB
-#define MOTOR_PWM_DDR DDRB
-#define MOTOR_CTL_PORT PORTD
-#define MOTOR_CTL_DDR DDRD
-
-#define M_AIN1 PD1
-#define M_AIN2 PD3
-#define M_BIN1 PD5
-#define M_BIN2 PD7
-
-// Left / Right defines (calculated)
-#define MOTOR_LEFT	M_PWMA
-#define MOTOR_RIGHT	M_PWMB
-
-#define M_LIN1 M_AIN1
-#define M_LIN2 M_AIN2
-#define M_RIN1 M_BIN1
-#define M_RIN2 M_BIN2
-
-#define MOTOR_L_FWD MOTOR_MODE_CW
-#define MOTOR_L_BWD MOTOR_MODE_CCW
-#define MOTOR_R_FWD MOTOR_MODE_CW
-#define MOTOR_R_BWD MOTOR_MODE_CCW
+// value's sign indicates direction (attinyX61 has 10bit pwm)
+typedef motor_speed_t int16_t;
 
 
