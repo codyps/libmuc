101e740fa88cca33c431205eb92a5eb860db36df
Eros <eros@eros-laptop.(none)>
2009-10-02 18:33:11 -0400

    common: queue: tidy up a bit and make assumptions about how the buffer is accessed

--- common/queue.h
+++ common/queue.h
@@ -8,19 +8,18 @@
 
 #define QUEUE_BASE_T	uint8_t
 #define QUEUE_INDEX_T	uint8_t
-#define QUEUE_SZ	64
 
 typedef volatile struct {
-	QUEUE_BASE_T * buffer;	// buffer
+	QUEUE_BASE_T *restrict buffer;	// buffer
 	QUEUE_INDEX_T first;	// position of first element
 	QUEUE_INDEX_T last;	// position of last element (should be eliminated)
 	QUEUE_INDEX_T ct;	//
 	const QUEUE_INDEX_T sz;	// size of the buffer
-	//uint8_t flags;      // for when i need to tack something on.
-}  queue_t;
+} queue_t;
 
 #define Q_DEF(buff) { .buffer = (buff), .first = 0, .last = 0, .ct = 0, .sz = sizeof(buff) / sizeof(QUEUE_BASE_T) }
 
+// adds a new element
 int8_t q_push(queue_t *q, QUEUE_BASE_T x);
 int8_t q_push_o(queue_t *q, QUEUE_BASE_T x); // overwrites old data on q full
 
