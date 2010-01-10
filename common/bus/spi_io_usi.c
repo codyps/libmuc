2c5557b83d44fb56a57fdc19df94996c6eff7856
jmesmon <jmesmon@gmail.com>
2010-01-10 14:53:40 -0500

    common: bus: spi_io_usi: fix formatting and switch to using the list type

--- common/bus/spi_io_usi.c
+++ common/bus/spi_io_usi.c
@@ -5,15 +5,19 @@ DO -> 1
 CLK-> 2
 */
 
+#include <avr/io.h>
+#include <avr/power.h>
+#include <avr/interrupt.h>
+
 static inline void spi_isr_on (void)  {USICR |= (1<<USIOIE);}
 static inline void spi_isr_off(void)  {USICR &= (uint8_t)~(1<<USIOIE);}
 
 static void spi_io_init_hw(void) {
-  power_usi_enable();
+	power_usi_enable();
 
 	// Clr the flags we interupt on + counter bits in same reg. 
 	//  (default is not raised)
-     //USISR = (1<<USIOIF);
+	//USISR = (1<<USIOIF);
  
 	// Clr datareg to avoid junk data being sent out. (defaults to 0)
 	//USIDR = 0;
@@ -23,54 +27,54 @@ static void spi_io_init_hw(void) {
 
 	#if ( SPI_PORT == B )
   	//#warning Using Port  B (default)
-    //USIPP &= (uint8_t)~(1<<USIPOS);
+	//USIPP &= (uint8_t)~(1<<USIPOS);
 	#else
   	#error Using Port  A   
   	USIPP |= (1<<USIPOS);
-  #endif
+	#endif
 	
-  #undef A
+	#undef A
 	#undef B
 
 	DDR(SPI_PORT) |= (1<<SPI_MISO);
 	DDR(SPI_PORT) &= (uint8_t)~((1<<SPI_MOSI)|(1<<SPI_CLK));
 	PORT(SPI_PORT)&= (uint8_t)~( (1<<SPI_MOSI) | (1<<SPI_CLK) | (1<<SPI_MISO) );
 
-  // To resolve SPI_EDGE
-  #define NEGATIVE 1
-  #define POSITIVE 0
+	// To resolve SPI_EDGE
+	#define NEGATIVE 1
+	#define POSITIVE 0
   
-  USICR = (0<<USISIE) |			// Start IE(2w), CLK edge IE(3w)
-          (1<<USIOIE) |				// Ovf interupt (to refil register)
-          (0<<USIWM1) | (1<<USIWM0) |	// 01 = 3w, 00 = disable, 10&11 = 2w
-          (1<<USICS1) | (SPI_EDGE<<USICS0) | // 10 = positive edge, 11 = neg.
-          (0<<USICLK) |				// 4bit timer : 0 = external, 1 = sw
-          (0<<USITC );				// Clock toggle
+	USICR = (0<<USISIE) | // Start IE(2w), CLK edge IE(3w)
+		(1<<USIOIE) | // Ovf interupt (to refil register)
+		(0<<USIWM1) | (1<<USIWM0) | // 01 = 3w, 00 = disable, 10&11 = 2w
+		(1<<USICS1) | (SPI_EDGE<<USICS0) | // 10 = positive edge, 11 = neg.
+		(0<<USICLK) | // 4bit timer : 0 = external, 1 = sw
+		(0<<USITC );  // Clock toggle
 
-  #undef NEGATIVE
-  #undef POSITIVE
+	#undef NEGATIVE
+	#undef POSITIVE
 }
 
 ISR( SIG_USI_OVERFLOW ) {
-     USISR = (1<<USIOIF); // Clear interupt flag and counter.
+	USISR = (1<<USIOIF); // Clear interupt flag and counter.
 	
 	//transmit (tx)
-     if (!q_empty(&tx)) {
-		USIDR = q_pop(&tx);
+	if (!list_empty(&tx)) {
+		USIDR = list_pop_back(&tx);
 	}
 	else USIDR = 0;
 
-     //recieve (rx)
+	//recieve (rx)
 	uint8_t in = USIBR;
-     if (in != 0 && !q_full(&rx)) {
-          q_push(&rx,in);
+	if (in != 0 && !list_full(&rx)) {
+	        list_push_front(&rx,in);
 		if (in == '\n')
 			spi_io_rx_nl++;
-     }
+	}
 
 	/*// Alternate if we don't care about losing old data.
 	if (USIBR != 0) {
-          q_push_o(&rx,USIBR);
-     }
+		list_push_front_o(&rx,USIBR);
+	}
 	*/
 }
