f9dfe4bd86243a9b80778332a85403f55d398292
jmesmon <jmesmon@gmail.com>
2010-05-20 23:18:13 -0400

    add servo init, and sem_trydec attempt

--- arr/main.c
+++ arr/main.c
@@ -4,15 +4,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 #include <avr/io.h>
 #include <avr/power.h>
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 
+#include <util/atomic.h>
 #include <util/parity.h> 
 #include <util/delay.h>
 
+#include "servo.h"
 #include "usart.h"
 
 /*
@@ -20,21 +24,37 @@ ISR(BADISR_vect){
 }
 */
 
-void init(void) {
+void init(void)
+{
 	power_all_disable();
 
 	usart_init();
+	servo_init();
 
 	sei();
   
 	printf_P(PSTR("\n\n[main init done]\n\n"));
 }
 
-int main(void) {
+bool static sem_trydec(uint8_t sem)
+{
+	bool ret = false;
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+		if (sem) {
+			sem--;
+			ret = true;
+		}
+	}
+	return ret;
+}
+
+int main(void)
+{
 	init();
 	for(;;) {
-		printf("Hello\n");
-		_delay_ms(100);
+		if (sem_trydec(usart_msg)) {
+			int x = getchar();
+		}
 	}
    	return 0;
 }
