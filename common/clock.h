fe6a59400a04f70bae010daee9cc0b1a64a448f2
jmesmon <jmesmon@gmail.com>
2010-05-22 21:32:03 -0400

    switch CLICKS_xS to TICKS_xS

--- common/clock.h
+++ common/clock.h
@@ -4,7 +4,11 @@
 #define MHz(x) ( x * 1000000 )
 #define KHz(x) ( x *    1000 )
 
-#define CLICKS_MS(_ms_) ( (uint16_t) ( F_CPU * (_ms_) / 1000 ) )
-#define CLICKS_US(_us_) ( (uint16_t) ( F_CPU / 1000000 * (_us_) ) ) // 1000000 = 1e6
+#define CLICKS_MS TICKS_MS
+#define CLICKS_US TICKS_US
+
+#define TICKS_MS(_ms_) ( (uint16_t) ( F_CPU * (_ms_) / 1000 ) )
+#define TICKS_US(_us_) ( (uint16_t) ( F_CPU / 1000000 * (_us_) ) ) // 1000000 = 1e6
+
 
 #endif
