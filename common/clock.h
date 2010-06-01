a13a406f0ed0389c926fee5f8941d14c7e875bd0
jmesmon <jmesmon@gmail.com>
2010-06-01 17:42:26 -0400

    clock: fix US_TICKS calculation

--- common/clock.h
+++ common/clock.h
@@ -10,5 +10,6 @@
 #define TICKS_MS(_ms_) ( (uint16_t) ( F_CPU * (_ms_) / 1000 ) )
 #define TICKS_US(_us_) ( (uint16_t) ( F_CPU / 1000000 * (_us_) ) ) // 1000000 = 1e6
 
+#define US_TICKS(_ticks_) ( (uint16_t) ( _ticks_ / (F_CPU / 1000 / 1000)  ) )
 
 #endif
