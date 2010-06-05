4ea793a3d84272adb00e29defcabde2bc15f00c9
jmesmon <jmesmon@gmail.com>
2010-06-04 23:51:18 -0400

    i2c-new: added all TW_ values to the TWI_vect's switch. now just need to impliment them.

--- arr/i2c.c
+++ arr/i2c.c
@@ -100,18 +100,39 @@ ISR(TWI_vect)
 	switch(tw_status) {
 	case TW_START:
 	case TW_REP_START:
-	case TW_MT_SLA_ACK:
-	case TW_MT_DATA_ACK:
-	case TW_MR_SLA_ACK:
-	case TW_MR_DATA_ACK:
 
-	case TW_MR_DATA_NACK:
-	case TW_MR_SLA_NACK:
-	case TW_MT_DATA_NACK:
+	case TW_MT_SLA_ACK:
 	case TW_MT_SLA_NACK:
+	case TW_MT_DATA_ACK:
+	case TW_MT_DATA_NACK:
+
+	//case TW_MT_ARB_LOST: // identical to the following:
+	case TW_MR_ARB_LOST:
+	
+	case TW_MR_SLA_ACK:
+	case TW_MR_SLA_NACK:
+
+	case TW_MR_DATA_ACK:
+	case TW_MR_DATA_NACK:
+	
+	case TW_ST_SLA_ACK:
+	case TW_ST_ARB_LOST_SLA_ACK:
+	case TW_ST_DATA_ACK:
+	case TW_ST_DATA_NACK:
+	case TW_ST_LAST_DATA:
+	case TW_SR_SLA_ACK:
+	case TW_SR_ARB_LOST_SLA_ACK:
+
+	case TW_SR_GCALL_ACK:
+	case TW_SR_ARB_LOST_GCALL_ACK:
+	case TW_SR_DATA_ACK:
+	case TW_SR_DATA_NACK:
+	case TW_SR_GCALL_DATA_ACK:
+	case TW_SR_GCALL_DATA_NACK:
+	case TW_SR_STOP:
+	case TW_NO_INFO:
 
 	case TW_BUS_ERROR:
-	case TW_MT_ARB_LOST:
 	default:
 		break;
 	}
