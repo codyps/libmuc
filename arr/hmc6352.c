08c5a2c3f4aa617480079e8ed143cdf460cd7c9b
John Mesmon <jmesmon@gmail.com>
2010-06-09 19:31:41 -0400

    beginings of hmc6352 driver. (these things need beter names)

---
+++ arr/hmc6352.c
@@ -0,0 +1,15 @@
+#include "i2c.h"
+
+static uint8_t w_buf[] = { 'r' /* 'g' */, 0 /* addr */}; /* read eeprom / read ram */
+static uint8_t r_buf[1];
+static struct i2c_msg msgs[] = {
+	{ HMC6352_ADDR_W, 0, sizeof(w_buf), w_buf },
+	{ HMC6352_ADDR_R, 0, sizeof(r_buf), r_buf }
+}
+static struct i2c_trans cmd = { msgs, sizeof(msgs), hmc6352_cb };
+
+
+void hmc6352_read_mem(void)
+{
+		
+}
