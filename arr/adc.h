70eb854f78b53ff0fcd1c8118efec087cef16e51
John Mesmon <jmesmon@gmail.com>
2010-08-19 21:07:12 -0400

    arr: initial adc support

---
+++ arr/adc.h
@@ -0,0 +1,15 @@
+#ifndef ADC_H_
+#define ADC_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "adc_conf.h"
+
+void adc_init(void);
+uint16_t adc_get_i(uint8_t sensor_i);
+
+extern uint16_t adc_values[ADC_CHANNEL_CT];
+extern volatile bool adc_new_data;
+
+#endif
