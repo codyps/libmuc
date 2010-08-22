e82b40fc747306bc69c8956a919727f3d28affe6
John Mesmon <jmesmon@gmail.com>
2010-08-22 02:27:57 -0400

    arr: add skeleton pid stuff.

---
+++ arr/pid.c
@@ -0,0 +1,8 @@
+
+#include <stdint.h>
+#include "pid.h"
+
+int16_t pid_update(struct pid_s *pid, int16_t cur_pos)
+{
+
+}
---
+++ arr/pid.h
@@ -0,0 +1,20 @@
+#ifndef PID_H_
+#define PID_H_
+
+
+struct pid_s {
+	uint16_t kp;
+	uint16_t kd;
+	uint16_t ki;
+
+	int16_t last;
+	int16_t accum;
+
+	int16_t set_point; /* goal */
+};
+
+
+#define PID_SETPOINT(pid, sp) (pid).set_point = (sp)
+int16_t pid_update(struct pid_s *pid, int16_t curr_point);
+
+#endif
