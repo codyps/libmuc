85e2c27fbc20c1061f7278bdeb3c8f7355756e4b
John Mesmon <jmesmon@gmail.com>
2010-10-06 01:35:03 -0400

    mctrl: add some led flashing.

---
+++ mctrl/error_led.h
@@ -0,0 +1,8 @@
+#ifndef ERROR_LED_H_
+#define ERROR_LED_H_ 1
+
+#include <stdint.h>
+void led_init(void);
+void led_flash(uint8_t i);
+
+#endif
--- mctrl/main.c
+++ mctrl/main.c
@@ -3,18 +3,22 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+
+#include "error_led.h"
 #include "frame_async.h"
 
 __attribute__((noreturn))
 void main(void)
 {
+	static uint8_t ct;
 	cli();
 	frame_init();
+	led_init();
 	sei();
-	static uint8_t ct;
 	for(;;) {
 		uint8_t *f = frame_recv();
 		if (f) {
+			led_flash(5);
 			ct = 0;
 			uint8_t flen = frame_recv_len();
 			frame_send(f, flen);
@@ -24,8 +28,9 @@ void main(void)
 			_delay_ms(50);
 		}
 
-		if (ct == 0xff) {
-			ct = 0;
+		if (ct == 0) {
+			ct++;
+			led_flash(3);
 			const char rdy_str[] = "hello";
 			frame_send(rdy_str, strlen(rdy_str));
 		}
