333475610f106b8cb4791fe5b17559618d27ea8f
John Mesmon <jmesmon@gmail.com>
2010-10-17 20:36:11 -0400

    common/ds: expand circ_buf

--- common/ds/test_circ_buf.c
+++ common/ds/test_circ_buf.c
@@ -8,7 +8,9 @@ void print_info(uint8_t head, uint8_t tail, uint8_t size)
 	printf("tail (removal   point): %d\n", tail);
 	printf("size : %d\n", size);
 	printf("space_to_end: %d\n", CIRC_SPACE_TO_END(head, tail, size));
-	printf("cnd_to_end:   %d\n", CIRC_CNT_TO_END(head, tail, size));
+	printf("cnt_to_end:   %d\n", CIRC_CNT_TO_END(head, tail, size));
+	printf("cnt:   %d\n", CIRC_CNT(head, tail, size));
+	printf("space:   %d\n", CIRC_SPACE(head, tail, size));
 }
 
 int main(int argc, char **argv)
