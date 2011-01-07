3485b16ce3975bf52e8aecc045350e25b7ae6e5f
Cody Schafer <cpschafer@gmail.com>
2011-01-06 22:50:16 -0500

    pid: add integral capping.

--- common/pid.h
+++ common/pid.h
@@ -7,6 +7,8 @@ struct pid {
 	int16_t ki;
 
 	int16_t integral;
+	int16_t integral_max;
+
 	int16_t previous_error;
 
 	int16_t set_point; /* goal */
@@ -15,10 +17,11 @@ struct pid {
 #define pid_set_goal(pid, sp) ((pid).set_point = (sp))
 #define pid_set_kp(pid, n_kp) ((pid).kp = (n_kp))
 #define pid_set_kd(pid, n_kd) ((pid).kd = (n_kd))
-#define pit_set_ki(pid, n_ki) ((pid).ki = (n_ki))
+#define pid_set_ki(pid, n_ki) ((pid).ki = (n_ki))
+#define pid_set_imax(pid, n_imax) ((pid).integral_max = (n_imax))
 
-#define PID_INITIALIZER(ikp, ikd, iki)           \
-	{ .kp = (ikp), .kd = (ikd), .ki = (iki) }
+#define PID_INITIALIZER(ikp, ikd, iki, imaxi)     \
+	{ .kp = (ikp), .kd = (ikd), .ki = (iki), .integral_max = (imaxi) }
 
 int16_t pid_update(struct pid *pid, int16_t delta_time, int16_t curr_point);
 
