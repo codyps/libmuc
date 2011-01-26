13352ff3e6808d3d1391126625cdd34d199943a5
Cody Schafer <cpschafer@gmail.com>
2011-01-25 19:05:56 -0500

    adc: add atmega328p support.

--- adc.c
+++ adc.c
@@ -74,8 +74,8 @@ void adc_init(void) {
 
 	}
 
-	/*
-	 * REFS2 REFS1 REFS0 Voltage Reference (VREF) Selection
+#if defined(__AVR_ATmega644P__)
+	/* REFS2 REFS1 REFS0 Voltage Reference (VREF) Selection
 	 * ->  X     0     0   VCC , disconnected from AREF.
 	 *     X     0     1   AREF pin, Internal Voltage Reference turned off.
 	 *     0     1     0   Internal 1.1V Voltage Reference.
@@ -85,10 +85,26 @@ void adc_init(void) {
 	 *     1     1     1   Internal 2.56V Voltage Reference w/ extern
 	 *                         bypass capacitor at AREF pin.
 	 */
-#if 0
 	ADMUX = (0<<REFS1) | (0<<REFS0)
 		| (0<<ADLAR) /* Left adjust output when 1 */
 		| (0<<MUX4) | (0<<MUX3) | (0<<MUX2) | (0<<MUX1) | (0<<MUX0);
+#elif defined(__AVR_ATmega328P__)
+	/* REFS1 REFS0 Voltage Reference Selection
+	 *     0     0 AREF, Internal Vref turned off
+	 * ->  0     1 AVCC with external capacitor at AREF pin
+	 *     1     0 Reserved
+	 *     1     1 Internal 1.1V Voltage Reference with external
+	 *             capacitor at AREF pin
+	 * NOTE: you had better have that capacitor.
+	 */
+	ADMUX = (0<<REFS1) | (1<<REFS0)
+		| (0<<ADLAR) /* Left adjust output when 1 */
+		| (0<<4) /* reserved */
+		| (0<<MUX3) | (0<<MUX2) | (0<<MUX1) | (0<<MUX0);
+
+
+#else
+# error "unsupported avr"
 #endif
 
 	ADCSRA = (1<<ADEN)  /* Enable ADC */
@@ -98,8 +114,8 @@ void adc_init(void) {
 		|(1<<ADIE)  /* Enable interrupt	*/
 		|ADC_PRESCALE_BITS; /* Prescale, 3 bits */
 
-	/*
-	 * ADTS2 ADTS1 ADTS0 Trigger Source
+#if defined(__AVR_ATmega644P__)
+	/* ADTS2 ADTS1 ADTS0 Trigger Source
 	 * ->  0     0     0   Free Running mode
 	 *     0     0     1   Analog Comparator
 	 *     0     1     0   External Interrupt Request 0
@@ -109,13 +125,27 @@ void adc_init(void) {
 	 *     1     1     0   Timer/Counter1 Overflow
 	 *     1     1     1   Watchdog Interrupt Request
 	 */
-#if 0
 	ADCSRB = (0<<BIN)   /* Bipolar input mode */
 		|(0<<GSEL)  /* Diff input Gain, (20x -> 32x, 1x -> 8x) */
 		|(0<<5)     /* Reserved, always read as 0 */
 		|(0<<REFS2)
 		|(0<<MUX5)
 		|(0<<ADTS2) | (0<<ADTS1) | (0<<ADTS0);
+#elif defined(__AVR_ATmega328P__)
+	/* ADTS2 ADTS1 ADTS0 Trigger Source
+	 * 0 0 0 Free Running mode
+	 * 0 0 1 Analog Comparator
+	 * 0 1 0 External Interrupt Request 0
+	 * 0 1 1 Timer/Counter0 Compare Match A
+	 * 1 0 0 Timer/Counter0 Overflow
+	 * 1 0 1 Timer/Counter1 Compare Match B
+	 * 1 1 0 Timer/Counter1 Overflow
+	 * 1 1 1 Timer/Counter1 Capture Event
+	 */
+	ADCSRB = (0 << ACME)
+		|(0 << ADTS2) | (0 << ADTS1) | (0 << ADTS0);
+#else
+# error "unsupported avr"
 #endif
 
 	adc_channel_set_from_index(adc_curr_chan_index);
