e401556962dfdafbfed137e77d78d7c0c621a9c7
Cody Schafer <cpschafer@gmail.com>
2011-03-05 02:14:34 -0500

    add min & max

--- muc.h
+++ muc.h
@@ -17,7 +17,15 @@
 #define PIN(x)	_PIN(x)
 #define _PIN(x) PIN##x
 
-#define MIN(x,y) (((x)<(y))?(x):(y))
+#define MAX(x, y) ((x) > (y)?(x):(y))
+#define MAX4(a, b, c, d) MAX(MAX(a,b),MAX(c,d))
+#define MAX6(a,b,c,d,e,f) MAX(MAX4(a,b,c,d),MAX(e,f))
+#define MAX8(a,b,c,d,e,f,g,h) MAX(MAX4(a,b,c,d),MAX4(e,f,g,h))
+
+#define MIN(x,y) ((x) < (y)?(x):(y))
+#define MIN4(a, b, c, d) MIN(MIN(a,b),MIN(c,d))
+#define MIN6(a,b,c,d,e,f) MIN(MIN4(a,b,c,d),MIN(e,f))
+#define MIN8(a,b,c,d,e,f,g,h) MIN(MIN4(a,b,c,d),MIN4(e,f,g,h))
 
 #define CT(x) ARRAY_SIZE(x)
 #define ARRAY_SIZE(x) (sizeof(x)/sizeof(*(x)))
